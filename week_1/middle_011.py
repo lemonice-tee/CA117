2167577f192b53e2c96865cb3cd040b033afc1fe
⋆｡˚ ☁︎ ˚｡  lemonice_tee⋆｡˚☽˚｡⋆ <145129046+lemonice-tee@users.noreply.github.com>
2024-04-03 13:29:31 +0100

    Add files via upload

--- week_1/middle_011.py
+++ week_1/middle_011.py
@@ -3,9 +3,9 @@
 import sys
 
 for line in sys.stdin:
-  line = line.strip().split()
-  for word in line:
-    if len(word) % 2 == 0:
-      print('No middle character!')
-    else:
-      print(word[len(word) // 2])
+  line = line.strip()
+  if len(line) % 2 != 0:
+    middle = line[len(line) // 2]
+    print(middle)
+  else:
+    print("No middle character!")
